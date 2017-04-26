de6905b38682bf7004e105c8379dd56614d3dd52
Michael Czechowski <mail@dailysh.it>
2017-04-26 19:00:08 +0200

    added scripts gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,29 @@
 'use strict';
 
-var gulp        = require('gulp')
-var sass        = require('gulp-sass')
-var sourcemaps  = require('gulp-sourcemaps')
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var sourcemaps = require('gulp-sourcemaps')
+var browserify = require('gulp-browserify');
 
 var config = {
     src: {
         baseDir: './src',
         stylesDir: '/scss',
+        stylesMainFile: '/main.scss',
+        scriptsDir: '/js',
+        scriptsMainFile: '/main.js'
     },
     dest: {
         baseDir: './web/assets/',
-        stylesDir: '/css'
+        stylesDir: '/css',
+        scriptsDir: '/js'
     }
 }
 
 gulp.task('sass', function() {
-    return gulp.src(config.src.baseDir + config.src.stylesDir + '/main.scss')
+    var src = config.src.baseDir + config.src.stylesDir + config.src.stylesMainFile
+
+    return gulp.src(src)
         // .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         // .pipe(sourcemaps.write(config.dest.baseDir + config.dest.stylesDir))
@@ -25,4 +32,20 @@ gulp.task('sass', function() {
 
 gulp.task('sass:watch', function() {
     gulp.watch(config.src.baseDir + config.src.stylesDir + '**/*.scss', ['sass'])
+})
+
+gulp.task('scripts', function() {
+    var src = config.src.baseDir + config.src.scriptsDir + config.src.scriptsMainFile
+    var dest = config.dest.baseDir + config.dest.scriptsDir
+    // Single entry point to browserify
+    gulp.src(src)
+        .pipe(browserify({
+            insertGlobals: true,
+            debug: true
+        }))
+        .pipe(gulp.dest(dest))
+});
+
+gulp.task('scripts:watch', function() {
+    gulp.watch(config.src.baseDir + config.src.scriptsDir + '**/*.js', ['scripts'])
 })
