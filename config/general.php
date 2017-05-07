5463e6bb01738a1a2e090414d6d6285bd1a20125
Michael Czechowski <mail@dailysh.it>
2017-05-07 10:57:31 +0200

    several fixes

--- config/general.php
+++ config/general.php
@@ -8,16 +8,16 @@
 
 return [
     // Base site URL
-    'siteUrl' => null,
+    'siteUrl' => getenv('GENERAL_SITE_URL'),
 
     // Default Week Start Day (0 = Sunday, 1 = Monday...)
-    'defaultWeekStartDay' => 0,
+    'defaultWeekStartDay' => 1,
 
     // Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
     'enableCsrfProtection' => true,
 
     // Whether "index.php" should be visible in URLs (true, false, "auto")
-    'omitScriptNameInUrls' => true,
+    'omitScriptNameInUrls' => false,
 
     // Control Panel trigger word
     'cpTrigger' => 'admin',
